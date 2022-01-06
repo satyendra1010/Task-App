2e0a27061828f372fa7baf4ebba969f36ae2cae8
satyendra1010 <74778992+satyendra1010@users.noreply.github.com>
2022-01-07 03:38:29 +0530

    Deployment error fixes.

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ if(PORT == null || PORT ==""){
 }
 
 // ... other app.use middleware 
-app.use(express.static('client/build'));
-app.get('*', (req, res) => { 
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+app.use(express.static(path.join(__dirname, "Task-list", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Task-list", "build", "index.html"));
 });
 console.log('Serving React App...');
 
